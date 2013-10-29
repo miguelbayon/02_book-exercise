37b65f6dea985545d573562efcd397bda7ca08bd
miguelbayon <miguel@bayon-portatil>
2013-10-29 22:02:08 +0100

    Modificado para prueba

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Insert your name here)
+ * @version (Insert today's date here)
  */
 class Book
 {
